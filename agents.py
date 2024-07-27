6365f3e6b8453d66409d4f6994eda76b326f2732
Elfsong <dumingzhex@gmail.com>
2024-07-27 06:24:50 +0000

    Update

--- agents.py
+++ agents.py
@@ -4,6 +4,8 @@
 # Date: 2024 / 07 / 26
 
 import utils
+import torch
+import transformers
 from openai import OpenAI
 
 class GPTAgent():
@@ -50,25 +52,30 @@ class DeepSeekAgent():
 
         return completion.choices[0].message.content
     
-
 class HuggingFaceAgent():
     def __init__(self, model_name, api_key) -> None:
         self.model_name = model_name
         self.api_key = api_key
-        
-        self.client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
+        self.pipeline = transformers.pipeline(
+            "text-generation",
+            model=self.model_name,
+            model_kwargs={"torch_dtype": torch.bfloat16},
+            device_map="auto",
+        )
         
     def prompt_generation(self, problem_description):
         prompt = utils.few_shot_prompt + "\n" + problem_description
         return prompt
-    
-    def code_generation(self, prompt) -> str:
-        completion = self.client.chat.completions.create(
-            model=self.model_name,
-            messages=[
-                {"role": "system", "content": "You are a coding export. You response in Pure Python code only (explicitly import all libraries). Consider each input is a string, so use 'eval' to parse these inputs, and use * to decouple arguments."},
-                {"role": "user", "content": prompt}
-            ],
-        )
 
-        return completion.choices[0].message.content
+    def code_generation(self, prompt) -> str:
+        messages = [
+            {"role": "system", "content": "You are a coding export. You response in Pure Python code only (explicitly import all libraries). Consider each input is a string, so use 'eval' to parse these inputs, and use * to decouple arguments."},
+            {"role": "user", "content": prompt},
+        ]
+
+        outputs = self.pipeline(
+            messages,
+            max_new_tokens=2048,
+        )
+        
+        return outputs[0]["generated_text"][-1]
